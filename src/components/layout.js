584c270d2d440c788cef3e0a7076eb95a7fb5a73
Kevin Matsuo <kmatsuo@ards-mbp-2.attlocal.net>
2020-05-18 12:26:03 -0500

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { IdentityContextProvider } from 'react-netlify-identity-widget'
 import './layout.css'
 
 const Layout = ({ children }) => (
-  <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com">
+  <IdentityContextProvider url="https://classic-jamstack-intro-auth.netlify.com">
     <header>
       <Link to="/">JAMstack App</Link>
     </header>
